b5cbcdbde2a75a4c91d2722b192d5c4c60db625d
VieleytaZen <93396111+VieleytaZen@users.noreply.github.com>
2023-09-07 10:03:53 +0700

    Create Ai-ChatGPT.js

---
+++ plugins/Ai-ChatGPT.js
@@ -0,0 +1,19 @@
+import { axios } from 'axios';
+  
+ let handler = async (m, { text, usedPrefix, command }) => { 
+     if(!text) return m.reply(`Masukkan teks yang mau ditanyakan ke OpenAI \nContoh: ${usedPrefix+command} Apa itu chat GPT?`) 
+   const response = await axios.get( 
+     `https://sh.xznsenpai.xyz/api/openai?text=${text}&apikey=Rippanteq`, 
+     { 
+       responseType: "json", 
+     } 
+   ); 
+   const v = response.data; 
+   m.reply(v.result); 
+ }; 
+ handler.help = ["ai"]; 
+ handler.tags = ["Ai"]; 
+ handler.command = /^(ai)$/i; 
+ handler.register = true; 
+
+export default handler;
