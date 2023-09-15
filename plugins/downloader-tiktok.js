b4ec07b001788fca8ef9ee46590c360eb6fe3190
VieleytaZen <93396111+VieleytaZen@users.noreply.github.com>
2023-09-15 14:16:37 +0700

    Update downloader-tiktok.js

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -6,7 +6,7 @@ import { tiktok } from "@xct007/frieren-scraper";
 
 let handler = async (m, { args, usedPrefix, command }) => {
     if (!args || !args[0]) return m.reply(`Input URL:\n${usedPrefix + command} https://www.tiktok.com/@initokyolagii/video/7189917930761506075`);
-    const data = await tiktok.v1(args[0])
+    const data = await tiktok(args[0])
     if (data.error) return m.reply(`${data.message}`);
     const buffVideo = Buffer.from(await (await fetch(data.play)).arrayBuffer())
     const buffAudio = Buffer.from(await (await fetch(data.music)).arrayBuffer())
